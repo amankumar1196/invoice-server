939a770bc39be1d8e60a8e086dea84691a634af7
Aarif <aarifh719@gmail.com>
2022-01-26 14:36:20 +0530

    add member route

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -1,24 +1,41 @@
+const express = require("express");
+const router = express.Router();
 const { authJwt } = require("../middleware");
-const controller = require("../controllers/user.controller");
+const controller = require("../controllers/UserController");
 
-module.exports = function(app) {
-  app.get("/api/test/all", controller.allAccess);
+module.exports = app => {
+  router.get("/", authJwt.verifyToken, controller.index);
+  router.get("/:id", authJwt.verifyToken, controller.show);
+  router.post("/", authJwt.verifyToken, controller.create);
+  router.put("/:id", authJwt.verifyToken, controller.update);
+  router.delete("/:id", authJwt.verifyToken, controller.delete);
 
-  app.get(
-    "/api/test/user",
-    [authJwt.verifyToken],
-    controller.userBoard
-  );
-
-  app.get(
-    "/api/test/mod",
-    [authJwt.verifyToken, authJwt.isModerator],
-    controller.moderatorBoard
-  );
-
-  app.get(
-    "/api/test/admin",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
-  );
+  app.use('/api/v1/members', router);
 };
+
+
+
+// const { authJwt } = require("../middleware");
+// const controller = require("../controllers/user.controller");
+
+// module.exports = function(app) {
+//   app.get("/api/test/all", controller.allAccess);
+
+//   app.get(
+//     "/api/test/user",
+//     [authJwt.verifyToken],
+//     controller.userBoard
+//   );
+
+//   app.get(
+//     "/api/test/mod",
+//     [authJwt.verifyToken, authJwt.isModerator],
+//     controller.moderatorBoard
+//   );
+
+//   app.get(
+//     "/api/test/admin",
+//     [authJwt.verifyToken, authJwt.isAdmin],
+//     controller.adminBoard
+//   );
+// };
